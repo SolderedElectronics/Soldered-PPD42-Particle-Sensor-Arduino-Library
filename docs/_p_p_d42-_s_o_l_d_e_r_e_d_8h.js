6863b8e094bc34fd0127312f11db267e3fd0df0b
Docs Robot <robot@example.com>
2023-02-14 15:53:09 +0000

    Committing documentation changes

---
+++ docs/_p_p_d42-_s_o_l_d_e_r_e_d_8h.js
@@ -0,0 +1,4 @@
+var _p_p_d42__s_o_l_d_e_r_e_d_8h =
+[
+    [ "PPD42", "class_p_p_d42.html", "class_p_p_d42" ]
+];
--- docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -1,7 +1,5 @@
 var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
     [ "PPD42-SOLDERED.cpp", "_p_p_d42-_s_o_l_d_e_r_e_d_8cpp.html", null ],
-    [ "PPD42-SOLDERED.h", "_p_p_d42-_s_o_l_d_e_r_e_d_8h.html", [
-      [ "PPD42", "class_p_p_d42.html", "class_p_p_d42" ]
-    ] ]
+    [ "PPD42-SOLDERED.h", "_p_p_d42-_s_o_l_d_e_r_e_d_8h.html", "_p_p_d42-_s_o_l_d_e_r_e_d_8h" ]
 ];
